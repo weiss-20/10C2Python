191af3121b222bf9788e52d2814323e07924fef1
weiss-20 <judit.monok.weiss@gmail.com>
2021-09-30 14:08:28 +0200

    Órai munka

--- 20210930/muveletek.py
+++ 20210930/muveletek.py
@@ -4,3 +4,41 @@ print(round(3.1415, 2))
 # hatványozás
 print(pow(2, 8))  #pow(alap, kitevő)
 #abszolútérték abs(mit)
+print(abs(-12))
+print(abs(12))
+print(min(12,7,9,45,3))
+print(max(12,7,9,45,3))
+# pi
+'''
+import math
+a=math.pi
+print(a)
+'''
+from math import * #program elején helyezzük el
+a=pi
+print(a)
+# négyzetgyök -sqrt
+print(sqrt(16))  #math.sqrt(miből vonok gyököt)
+# felfelé kerekítés - math.ceil(mit kerekítek)
+print(ceil(3.16))
+#lefelé kerekítés math.floor(mit)
+print(floor(3.56))
+
+# 1. négyzet kerülete, területe
+a=float(input("Kérem a négyzet oldalát! a="))
+k=4*a
+t=pow(a,2)
+print("a(z)",a,"egység oldalú négyzet kerülete", k,"területe",t)
+print("A négyzet kerülete",k)
+print("A négyzet területe",t)
+# 2. téglalap kerülete, területe
+
+
+# 3. kör kerülete (2*r*PI), területe (r^2*PI)
+
+
+# 4. kocka térfogata (V=a^3), felszíne (A=6*a^2)
+
+
+#5. derékszögű háromszög 2 befogóját kérd be, add meg az átfogót! 
+#   c=négyzetgyök(a^2+b^2)
